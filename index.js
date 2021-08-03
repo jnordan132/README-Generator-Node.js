b94b7685bfef1b2b2d49f8968557d4d741039e05
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-08-03 12:47:13 -0400

    added questions array

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Give a summary of your project'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What dependencies did you install for this project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is your project used for?'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What kind of license does your porject need?'
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'Who contributed to this project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How did you test your app?'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'what is your GitHub?'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'what is your email?'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => {
+
+}
 
 // TODO: Create a function to initialize app
 function init() {}
